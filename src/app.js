daa878fdac65c9321095ae69f8129338e3b8070b
ttesfaye629@gmail.com <127722108+Tbe24@users.noreply.github.com>
2025-02-15 11:49:11 +0300

    delete and update api

--- src/app.js
+++ src/app.js
@@ -38,7 +38,27 @@ app.get('/feed', async (req, res) => {
     res.status(400).send("something went wrong")
   }
 });
-
+//delete user by userId from database
+app.delete('/user', async (req, res) => {
+  const userId = req.body.userId;
+  try {
+    const users = await User.findByIdAndDelete(userId);
+    res.send("User deleted successfully");
+  }catch (err) {
+    res.status(400).send("something went wrong")
+  }
+});
+//update user by userId using patch method
+app.patch('/user', async (req, res) => {
+  const userId = req.body.userId;
+  const data= req.body;
+  try {
+    const users = await User.findByIdAndUpdate({_id:userId}, data);
+    res.send("User updated successfully");
+  }catch (err) {
+    res.status(400).send("something went wrong")
+  }
+});
 connectDB()
 .then(() => {
   console.log("Connected to MongoDB");
